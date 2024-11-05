429a3a98b29f508e4327a9511b2b3a950235c511
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-11-05 17:18:54 -0600

    Added dependency of serde. Refactored get_access_token a little.

---
+++ src/access_token.rs
@@ -0,0 +1,44 @@
+use core::panic;
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AccessToken {
+    username: String,
+    access_token: String,
+}
+
+// URL to send POST to and get access token
+static URL: &str = "https://test.api.amadeus.com/v1/security/oauth2/token";
+
+pub async fn get_access_token() -> Result<String, Box<dyn std::error::Error>> {
+    println!("Getting Access Token:");
+
+    // Getting env variables to obtain AMAD access token
+    let amad_api = env::var("AMAD_API_KEY").unwrap_or_else(|error| {
+        panic!("AMAD API Key not found in environment variables: {}", error)
+    });
+    let amad_secret = env::var("AMAD_API_SECRET").unwrap_or_else(|error| {
+        panic!(
+            "AMAD API Secret not found in environment variables: {}",
+            error
+        )
+    });
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post(URL)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(format!(
+            "grant_type=client_credentials&client_id={}&client_secret={}",
+            amad_api, amad_secret
+        ))
+        .send()
+        .await?;
+
+    let body = res.text().await?;
+    let token: AccessToken = serde_json::from_str(&body)?;
+
+    Ok(token.access_token)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
-use core::panic;
-use reqwest;
-use std::env;
+use access_token::get_access_token;
+
+mod access_token;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
-    // Getting env variables to obtain AMAD access token
-    let amad_api = env::var("AMAD_API_KEY").unwrap_or_else(|error| {
-        panic!("AMAD API Key not found in environment variables: {}", error)
-    });
-    let amad_secret = env::var("AMAD_API_SECRET").unwrap_or_else(|error| {
-        panic!(
-            "AMAD API Secret not found in environment variables: {}",
-            error
-        )
-    });
-
-    let client = reqwest::Client::new();
-    let res = client
-        .post("https://test.api.amadeus.com/v1/security/oauth2/token")
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(format!(
-            "grant_type=client_credentials&client_id={}&client_secret={}",
-            amad_api, amad_secret
-        ))
-        .send()
-        .await?;
-
-    println!("status: {}", res.status());
-    let body = res.text().await?;
-    println!("Response body: {}", body);
+    let access_token = get_access_token().await?;
+    println!("Acess token: {access_token}");
 
     Ok(())
 }
